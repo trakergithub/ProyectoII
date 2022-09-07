36388e8cedf11316531e57ff083badf067862897
Traker <trakerdroid@gmail.com>
2022-09-06 23:31:48 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import logo from './img/logo.png';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <link href="https://fonts.googleapis.com/css2?family=Baloo+2&family=Sacramento&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Baloo+2&family=Sacramento&display=swap" rel="stylesheet"></link>
         <img src={logo} className="App-logo" alt="logo" />
         <p class="titulo">
-          Utstal
+          Healt<br></br>&nbsp;&nbsp;&<br></br>Care
         </p>
+        <br></br>
         <span class="texto">
           <em>
-          Todos los días tendrás que tomar miles de decisiones
+          Todos los días tendrás que tomar miles 
           <br></br>
-          y siempre tendrás más de una alternativa 
+          de decisiones
+          y siempre tendrás más
           <br></br>
-          y entre ellas esta vivir en paz.
+          de una alternativa 
+          y entre ellas esta
+          <br></br>
+          vivir en paz.
           </em>
+          <br></br><br></br>
+          Bienvenido 
           <br></br>
-          Bienvenido acabas de tomar tu primera decisión.
-          <br></br>
-          Entrar
+          acabas de tomar tu primera decisión.
+          <br></br><br></br>
+          <a class="Boton">
+            Entrar
+          </a>
+          <br></br><br></br>
         </span>
       </header>
     </div>
